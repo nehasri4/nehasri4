d371eb90a9229c825c332ba65fb9cfd0380eecd3
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2024-07-22 14:42:15 +0530

    swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+#python program to swap two numbers with using a temporaryvariable
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+b=a+b
+a=b-a
+b=b-a
+print("numbers after swapping: ",a,b)
