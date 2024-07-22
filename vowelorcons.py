da3baeec08d58a24830dc10e4a867aec78a76235
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2024-07-22 16:37:13 +0530

    vowelorcons.py

---
+++ vowelorcons.py
@@ -0,0 +1,6 @@
+#python program to find given alphabet is vowel or consonant
+ch = input('enter the alphabet: ')
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+    print('alphabet is vowel')
+else:
+    print('alphabet is consonant')
